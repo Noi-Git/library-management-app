33082946fe23802d43a98d690e1366f9f24649d3
Noi-Git <noi.patsin@gmail.com>
2024-04-24 16:54:38 -0400

    setup home page

--- lb-frontend/src/App.js
+++ lb-frontend/src/App.js
@@ -1,13 +1,16 @@
 import { Routes, Route } from 'react-router-dom'
 import Container from 'react-bootstrap/Container'
 import Navigation from './components/nav/Navigation'
+import Home from './components/pages/home/Home'
 
 function App() {
   return (
     <div className='app-bg'>
       <Navigation />
       <Container>
-        <Routes></Routes>
+        <Routes>
+          <Route exact path='/' element={<Home />} />
+        </Routes>
       </Container>
     </div>
   )
