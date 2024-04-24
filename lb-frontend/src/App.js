e0c8a7ad9a9f7d907028ec4b3c67db2fe861e168
Noi-Git <noi.patsin@gmail.com>
2024-04-24 16:57:54 -0400

    create login route

--- lb-frontend/src/App.js
+++ lb-frontend/src/App.js
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom'
 import Container from 'react-bootstrap/Container'
 import Navigation from './components/nav/Navigation'
 import Home from './components/pages/home/Home'
+import Login from './components/auth/Login'
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Container>
         <Routes>
           <Route exact path='/' element={<Home />} />
+          <Route path='/login' element={<Login />} />
         </Routes>
       </Container>
     </div>
