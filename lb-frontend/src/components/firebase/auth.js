e453f5fd4af1f666bc3db8794d07223cd670fa0a
Noi-Git <noi.patsin@gmail.com>
2024-04-25 00:00:47 -0400

    user sign-in

--- lb-frontend/src/components/firebase/auth.js
+++ lb-frontend/src/components/firebase/auth.js
@@ -1,6 +1,22 @@
 import { auth } from './firebase'
-import { createUserWithEmailAndPassword } from 'firebase/auth'
+import {
+  createUserWithEmailAndPassword,
+  GoogleAuthProvider,
+  signInWithPopup,
+} from 'firebase/auth'
 
+//create new user with email and password
 export const doCreateUserWithEmailAndPassword = async (email, password) => {
   return createUserWithEmailAndPassword(auth, email, password)
+}
+
+//sign-in with email and password
+export const doSignInWithGoogle = async () => {
+  const provider = new GoogleAuthProvider(auth)
+  //popup will open and allow for sign-in with Gmail
+  const result = await signInWithPopup(auth, provider)
+
+  //you can return user and save user information to file store
+  //result.user
+  return result
 }
