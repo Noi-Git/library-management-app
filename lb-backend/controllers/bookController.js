67eef6a3a2b910d04b9cf6ff8525997f3457079e
Noi-Git <noi.patsin@gmail.com>
2024-04-26 09:45:08 -0400

    modify get a single book route

--- lb-backend/controllers/bookController.js
+++ lb-backend/controllers/bookController.js
@@ -14,8 +14,9 @@ export const getBooks = (req, res) => {
 
 export const getBook = (req, res) => {
   res.json('from book controller')
-  const q =
-    'SELECT `book_title`, `book_description`, `book_image_url`, FROM books b JOIN users u ON u.user_id === b.book_id WHERE b.book_id = ?'
+  // const q =
+  //   'SELECT `user_firstname`, `user_lastname`, `book_title`, `book_description`, `book_image_url`, FROM users u JOIN books b ON u.user_id = b.book_id WHERE b.book_id = ?'
+  const q = 'SELECT * FROM books WHERE book_id = ?'
 
   db.query(q, [req.params.id], (err, data) => {
     if (err) return res.json(err)
