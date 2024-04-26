9d0058f4fdab7d6544f82134e9a8699bae5fd939
Noi-Git <noi.patsin@gmail.com>
2024-04-25 22:05:19 -0400

    setup redux store

---
+++ lb-frontend/src/redux/store.js
@@ -0,0 +1,9 @@
+import { configureStore } from '@reduxjs/toolkit'
+
+// import rootReducer from './reducers'
+
+export const store = configureStore({
+  reducer: {},
+})
+
+// The store now has redux-thunk added and the Redux DevTools Extension is turned on
