a2572d7dcaa7899249e31f35d9e7a63d55a511e6
Noi-Git <noi.patsin@gmail.com>
2024-04-27 12:51:39 -0400

    add inner join query

--- lb-db/command.sql
+++ lb-db/command.sql
@@ -9,3 +9,8 @@ SHOW COULUMNS FROM books;
 
 
 
+SELECT book_id, book_title, book_description, book_image_url, total_copies, author_first_name, author_last_name, author_middlename, genre_name
+FROM books
+INNER JOIN authors ON books.author_id = authors.author_id
+INNER JOIN genre ON books.genre_id = genre.genre_id;
+
