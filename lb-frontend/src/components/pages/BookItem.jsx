1189857fe57b8acf809a9501d2f6d835c4eed1e8
Noi-Git <noi.patsin@gmail.com>
2024-04-27 12:51:59 -0400

    adjust style

--- lb-frontend/src/components/pages/BookItem.jsx
+++ lb-frontend/src/components/pages/BookItem.jsx
@@ -2,29 +2,33 @@ import { Link } from 'react-router-dom'
 // import default_product from '../../assets/images/default_product.png'
 
 const BookItem = ({ children, book }) => {
-  const { book_id, book_title, book_description, author_id, book_image_url } =
-    book
+  const {
+    book_id,
+    book_title,
+    book_image_url,
+    author_firstname,
+    author_lastname,
+    author_middlename,
+    genre_name,
+  } = book
   const elipsis = 60
 
   return (
     <>
-      <div class='col-sm-12 col-md-6 col-lg-3 my-3' key={book_id}>
-        <div class='card p-3 rounded'>
-          <img class='card-img-top mx-auto' src={book_image_url} alt='' />
-          <div class='card-body d-flex flex-column'>
-            <h5 class='card-title'>
+      <div className='col-sm-12 col-md-6 col-lg-3 my-3' key={book_id}>
+        <div className='card p-3 rounded'>
+          <img className='card-img-top mx-auto' src={book_image_url} alt='' />
+          <span className='text-center'>{genre_name}</span>
+          <div className='card-body d-flex flex-column'>
+            <h5 className='card-title'>
               {book_title.slice(0, elipsis)}
               {book_title.length > elipsis ? '...' : ''}
             </h5>
           </div>
-          {/* <p class='card-text'>
-            {book_description.slice(0, 100)}
-            <span className='read-more'> Read more...</span>
-          </p> */}
-          <span id='no_of_reviews' class='pt-2 ps-2'>
-            {author_id}
+          <span className='pb-2'>
+            {author_firstname} {author_middlename} {author_lastname}
           </span>
-          <Link to='' id='view_btn' class='btn btn-block'>
+          <Link to='' id='view_btn' className='btn btn-block'>
             View Details
           </Link>
         </div>
