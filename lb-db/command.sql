8288bb6815833d82e78786960e8bc75304e11532
Noi-Git <noi.patsin@gmail.com>
2024-04-28 00:40:33 -0400

    pagination query

--- lb-db/command.sql
+++ lb-db/command.sql
@@ -14,3 +14,25 @@ FROM books
 INNER JOIN authors ON books.author_id = authors.author_id
 INNER JOIN genre ON books.genre_id = genre.genre_id;
 
+
+-- set up pagination in database
+-- PROCEDURE is similar to function in js
+-- CREATE DEFINER=`root`@`localhost` PROCEDURE `Get_Pagination` (
+CREATE PROCEDURE Get_Pagination (
+  page_size INT,
+  page_index INT
+)
+
+BEGIN
+  DECLARE skip_records INT;
+  SET skip_records = (page_size * (page_index - 1));
+
+  SELECT * FROM books
+  ORDER BY book_title
+  LIMIT page_size
+  OFF SET skip_records;
+
+  SELECT page_size, page_index, COUNT(*) AS TotalCount, CEIL(COUNT(*) / page_size) AS TotalPage
+  FROM books;
+END
+
