eefd255ccb9cfd7e1807c235eabdd66f437a711b
Noi-Git <noi.patsin@gmail.com>
2024-04-28 11:43:40 -0400

    Add wait for connection when connecting to mysql

--- lb-backend/app.js
+++ lb-backend/app.js
@@ -24,6 +24,8 @@ export const db = mysql.createConnection({
   user: process.env.DATABASE_USER,
   password: process.env.DATABASE_PASSWORD,
   database: process.env.DATABASE_NAME,
+  waitForConnection: true,
+  queueLimit: 0,
 })
 
 //register route
