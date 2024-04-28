b849ad195f84d3eac49a289263253dc1bc23b8c7
Noi-Git <noi.patsin@gmail.com>
2024-04-28 09:51:38 -0400

    add search by description author and genre

--- lb-backend/controllers/searchController.js
+++ lb-backend/controllers/searchController.js
@@ -1,9 +1,61 @@
 import { db } from '../app.js'
 
-export const searchByTitle = (req, res) => {
+export const searchTitle = (req, res) => {
   let search_query = req.query.search_query
   const sql = `SELECT book_title FROM books
   WHERE book_title LIKE '%${search_query}%'
+  LIMIT 10`
+
+  db.query(sql, (err, data) => {
+    if (err) res.status(404).json(err)
+
+    res.status(200).json(data)
+  })
+}
+
+export const searchDescription = (req, res) => {
+  let search_query = req.query.search_query
+  const sql = `SELECT book_description FROM books
+  WHERE book_description LIKE '%${search_query}%'
+  LIMIT 10`
+
+  db.query(sql, (err, data) => {
+    if (err) res.status(404).json(err)
+
+    res.status(200).json(data)
+  })
+}
+
+export const searchAuthorFirstname = (req, res) => {
+  let search_query = req.query.search_query
+  const sql = `SELECT author_firstname FROM authors
+  WHERE author_firstname LIKE '%${search_query}%'
+  LIMIT 10`
+
+  db.query(sql, (err, data) => {
+    if (err) res.status(404).json(err)
+
+    res.status(200).json(data)
+  })
+}
+
+export const searchAuthorlastname = (req, res) => {
+  let search_query = req.query.search_query
+  const sql = `SELECT author_lastname FROM authors
+  WHERE author_lastname LIKE '%${search_query}%'
+  LIMIT 10`
+
+  db.query(sql, (err, data) => {
+    if (err) res.status(404).json(err)
+
+    res.status(200).json(data)
+  })
+}
+
+export const searchGenre = (req, res) => {
+  let search_query = req.query.search_query
+  const sql = `SELECT genre_name FROM genre
+  WHERE genre_name LIKE '%${search_query}%'
   LIMIT 10`
 
   db.query(sql, (err, data) => {
--- lb-backend/routes/search.js
+++ lb-backend/routes/search.js
@@ -1,8 +1,18 @@
 import express from 'express'
-import { searchByTitle } from '../controllers/searchController.js'
+import {
+  searchAuthorFirstname,
+  searchAuthorlastname,
+  searchDescription,
+  searchGenre,
+  searchTitle,
+} from '../controllers/searchController.js'
 
 const searchRouter = express.Router()
 
-searchRouter.get('/title', searchByTitle)
+searchRouter.get('/title', searchTitle)
+searchRouter.get('/description', searchDescription)
+searchRouter.get('/author_firstname', searchAuthorFirstname)
+searchRouter.get('/author_lastname', searchAuthorlastname)
+searchRouter.get('/genre', searchGenre)
 
 export default searchRouter
