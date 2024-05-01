e32613a63edb62bdcda0cc46c0ed88bc79d42814
Noi-Git <noi.patsin@gmail.com>
2024-04-30 23:56:03 -0400

    still doesn't work

--- lb-frontend/src/components/auth/Register.jsx
+++ lb-frontend/src/components/auth/Register.jsx
@@ -1,9 +1,8 @@
 import { useState } from 'react'
 import 'react-toastify/dist/ReactToastify.css'
 import { Row, Col } from 'react-bootstrap'
-import { sendSignInLinkToEmail } from 'firebase/auth'
+import { getAuth, sendSignInLinkToEmail } from 'firebase/auth'
 import book from '../../assets/images/book1x.png'
-// import axios from 'axios'
 import MetaData from '../layout/MetaData'
 import { auth } from '../firebase/firebase'
 import { toast } from 'react-toastify'
@@ -14,17 +13,30 @@ const Register = () => {
   const handleSubmit = async (e) => {
     // console.log('register page---', email)
     e.preventDefault()
+    // const auth = getAuth();
     const config = {
       url: process.env.REACT_APP_REGISTER_REDIRECT_URL,
       handleCodeInApp: true,
     }
     console.log('config---', config)
     await sendSignInLinkToEmail(auth, email, config)
+      .then(() => {
+        // The link was successfully sent. Inform the user.
+        // Save the email locally so you don't need to ask the user for it again
+        // if they open the link on the same device.
+        window.localStorage.setItem('emailForSignIn', email)
+        // ...
+      })
+      .catch((error) => {
+        console.log(error.code)
+        console.log(error.message)
+        // ...
+      })
     toast.success(
       `Email is sent to ${email}. Click the link to complete your registration.`
     )
     // save user email in local storage
-    window.localStorage.setItem('emailForRegistration', email)
+    // window.localStorage.setItem('emailForRegistration', email)
     // clear stat
     setEmail('')
   }
