18bd1f3fa8375637ccaaa4c17d655b3ba37c6cbf
Noi-Git <noi.patsin@gmail.com>
2024-05-02 22:10:16 -0400

    create authApi path and reducer

--- lb-frontend/src/App.js
+++ lb-frontend/src/App.js
@@ -4,16 +4,17 @@ import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 import Home from './components/pages/Home'
 import Login from './components/auth/Login'
-import Register from './components/auth/Register'
+// import Register from './components/auth/Register'
 import Header from './components/layout/Header'
 import Footer from './components/layout/Footer'
 // import { Toaster } from 'react-hot-toast'
-import './custom.scss'
-import './App.css'
 import BookDetails from './components/pages/books/BookDetails'
 import AddBook from './components/pages/admin/AddBook'
 import Genre from './components/pages/admin/Genre'
 import EmailPasswordRegister from './components/auth/EmailPasswordRegister'
+import LoginNew from './components/auth/LoginNew'
+import './custom.scss'
+import './App.css'
 
 function App() {
   return (
@@ -25,6 +26,7 @@ function App() {
           <Route exact path='/' element={<Home />} />
           <Route path='/book/:id' element={<BookDetails />} />
           <Route path='/login' element={<Login />} />
+          <Route path='/login-new' element={<LoginNew />} />
           <Route path='/register' element={<EmailPasswordRegister />} />
           {/* <Route path='/register' element={<Register />} /> */}
           <Route path='/add-book' element={<AddBook />} />
